401019dc0047ac53e6757c6b779e335169419e40
Conark245 <trythkk@gmail.com>
2026-08-14 06:30:14 -0700

    Update

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,4 @@
-import { createExpressApp } from '../server.ts';
-
-const app = createExpressApp();
-
+// @ts-ignore
+import serverPkg from '../dist/server.cjs';
+const app = serverPkg.createExpressApp();
 export default app;
